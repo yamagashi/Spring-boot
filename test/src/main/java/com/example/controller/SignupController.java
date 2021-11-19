bf78d0d9c75d4a79dab3bb05fb3041860e5fc196
yamagashi <90173762+yamagashi@users.noreply.github.com>
2021-11-19 16:31:48 +0900

    commit

--- test/src/main/java/com/example/controller/SignupController.java
+++ test/src/main/java/com/example/controller/SignupController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,9 @@ public class SignupController {
 	private UserApplicationService userApplicationService;
 
 	@GetMapping("/signup")
-	public String getSignup(Model model) {
+	public String getSignup(Model model,Locale locale) {
 
-		Map<String,Integer> genderMap=userApplicationService.getGenderMap();
+		Map<String,Integer> genderMap=userApplicationService.getGenderMap(locale);
 		model.addAttribute("genderMap",genderMap);
 
 		return "user/signup";
